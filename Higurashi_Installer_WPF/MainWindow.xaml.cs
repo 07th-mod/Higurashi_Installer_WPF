67034b775171bdf6ef95eb1a5352a6318f548514
Vocalnight <vocalebooks@gmail.com>
2018-03-29 17:13:12 -0300

    Added functionality to the dropbox. Dropbox is done.

--- Higurashi_Installer_WPF/MainWindow.xaml.cs
+++ Higurashi_Installer_WPF/MainWindow.xaml.cs
@@ -16,17 +16,15 @@ using System.Windows.Shapes;
 
 namespace Higurashi_Installer_WPF
 {
-    /// <summary>
-    /// Interação lógica para MainWindow.xam
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         PatcherPOCO patcher = new PatcherPOCO();
 
         public MainWindow()
         {
-            InitializeComponent();
-            ChkPatch.DataContext = this;
+            InitializeComponent();            
+            patcher.IsFull = true;
         }
 
         private void BtnOnikakushi_Click(object sender, RoutedEventArgs e)
@@ -41,7 +39,7 @@ namespace Higurashi_Installer_WPF
                 }
             }
             MainImage.Source = new BitmapImage(new Uri("/Resources/header2.jpg", UriKind.Relative));
-
+            patcher.ChapterName = "Onikakushi";
         }
 
         private void BtnWatanagashi_Click(object sender, RoutedEventArgs e)
@@ -56,8 +54,7 @@ namespace Higurashi_Installer_WPF
                 }
             }
             MainImage.Source = new BitmapImage(new Uri("/Resources/header1.jpg", UriKind.Relative));
-
-
+            patcher.ChapterName = "Watanagashi";
         }
 
         private void BtnTatarigoroshi_Click(object sender, RoutedEventArgs e)
@@ -72,6 +69,7 @@ namespace Higurashi_Installer_WPF
                 }
             }
             MainImage.Source = new BitmapImage(new Uri("/Resources/header3.jpg", UriKind.Relative));
+            patcher.ChapterName = "Tatarigoroshi";
         }
 
         private void BtnHimatsubushi_Click(object sender, RoutedEventArgs e)
@@ -85,6 +83,7 @@ namespace Higurashi_Installer_WPF
                 }
             }
             MainImage.Source = new BitmapImage(new Uri("/Resources/header4.jpg", UriKind.Relative));
+            patcher.ChapterName = "Himatsubushi";
         }
 
         private void BtnMeakashi_Click(object sender, RoutedEventArgs e)
@@ -98,17 +97,28 @@ namespace Higurashi_Installer_WPF
                 }
             }
             MainImage.Source = new BitmapImage(new Uri("/Resources/header5.jpg", UriKind.Relative));
+            patcher.ChapterName = "Meakashi";
         }
 
-        private void ResetLayout()
-        {
-            MainGrid.Visibility = Visibility.Visible;
-            InstallGrid.Visibility = Visibility.Collapsed;
-        }
-
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-
+        private void InstallCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {          
+            switch (InstallCombo.SelectedItem.ToString().Split(new string[] { ": " }, StringSplitOptions.None).Last()) {
+                case "Full":
+                    ResetDropBox();
+                    patcher.IsFull = true;
+                    TreatCheckboxes(false);
+                    break;
+                case "Custom":
+                    ResetDropBox();
+                    patcher.IsCustom = true;
+                    TreatCheckboxes(true);
+                    break;
+                case "Voice only":
+                    ResetDropBox();
+                    patcher.IsCustom = true;
+                    TreatCheckboxes(false);
+                    break;
+            }
         }
 
         private void BtnInstall_Click(object sender, RoutedEventArgs e)
@@ -116,6 +126,32 @@ namespace Higurashi_Installer_WPF
 
         }
 
+        private void ResetDropBox()
+        {
+            patcher.IsVoiceOnly = false;
+            patcher.IsCustom = false;
+            patcher.IsFull = false;
+        }
+
+        private void TreatCheckboxes(Boolean IsCustom)
+        {
+            if (IsCustom)
+            {
+                ChkPatch.Visibility = Visibility.Visible;
+                ChkPS3.Visibility = Visibility.Visible;
+                ChkSteamSprites.Visibility = Visibility.Visible;
+                ChkUI.Visibility = Visibility.Visible;
+                ChkVoices.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                ChkPatch.Visibility = Visibility.Collapsed;
+                ChkPS3.Visibility = Visibility.Collapsed;
+                ChkSteamSprites.Visibility = Visibility.Collapsed;
+                ChkUI.Visibility = Visibility.Collapsed;
+                ChkVoices.Visibility = Visibility.Collapsed;
+            }
+        }
     }
 
     public static class WindowUtilties
